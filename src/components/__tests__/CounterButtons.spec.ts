c6a339ec82e8e69f30b18acf52ac1938a6b45610
Doic <dauhik@gmail.com>
2024-02-20 18:01:20 +0100

    Buttons and Display tests

---
+++ src/components/__tests__/CounterButtons.spec.ts
@@ -0,0 +1,65 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import CounterDisplay from '../CounterDisplay.vue'
+import CounterButtons from '../CounterButtons.vue'
+
+describe('CounterButtons', () => {
+  const displayWrapper = mount(CounterDisplay)
+  const buttonsWrapper = mount(CounterButtons)
+  const incrementButton = buttonsWrapper.find<HTMLButtonElement>('button.increment')
+  const decrementButton = buttonsWrapper.find<HTMLButtonElement>('button.decrement')
+
+  const displayWrapperValue5 = mount(CounterDisplay, { props: { counterId: 'value5', value: '5' } })
+  const buttonsWrapperValue5 = mount(CounterButtons, { props: { counterId: 'value5' } })
+  const incrementButtonValue5 = buttonsWrapperValue5.find<HTMLButtonElement>('button.increment')
+  const decrementButtonValue5 = buttonsWrapperValue5.find<HTMLButtonElement>('button.decrement')
+
+  const displayWrapperRange = mount(CounterDisplay, {
+    props: { counterId: 'range', value: '5', min: '4', max: '6' }
+  })
+  const buttonsWrapperRange = mount(CounterButtons, { props: { counterId: 'range' } })
+
+  it('render properly with no CounterDisplay props', () => {
+    expect(buttonsWrapper.find('button.increment').exists()).toBe(true)
+    expect(buttonsWrapper.find('button.decrement').exists()).toBe(true)
+  })
+  it('correctly increment and decrement displayed value', async () => {
+    await buttonsWrapperValue5.find('button.decrement').trigger('click')
+    expect(displayWrapperValue5.text()).toBe('4')
+    await buttonsWrapperValue5.find('button.increment').trigger('click')
+    expect(displayWrapperValue5.text()).toBe('5')
+  })
+  it('correctly disabled when out of range', async () => {
+    const incrementButtonRange = buttonsWrapperRange.find<HTMLButtonElement>('button.increment')
+    const decrementButtonRange = buttonsWrapperRange.find<HTMLButtonElement>('button.decrement')
+    await decrementButtonRange.trigger('click')
+    expect(displayWrapperRange.text()).toBe('4')
+    expect(decrementButtonRange.element.disabled).toBe(true)
+    await incrementButtonRange.trigger('click') // 5
+    await incrementButtonRange.trigger('click') // 6
+    expect(displayWrapperRange.text()).toBe('6')
+    expect(incrementButtonRange.element.disabled).toBe(true)
+    await decrementButtonRange.trigger('click') // 5
+  })
+
+  it('correctly increment and decrement value with same counterId', async () => {
+    await incrementButton.trigger('click')
+    await incrementButton.trigger('click')
+    expect(displayWrapper.text()).toBe('2')
+    expect(displayWrapperValue5.text()).toBe('5')
+
+    await decrementButton.trigger('click')
+    expect(displayWrapper.text()).toBe('1')
+    expect(displayWrapperValue5.text()).toBe('5')
+
+    await incrementButtonValue5.trigger('click')
+
+    expect(displayWrapper.text()).toBe('1')
+    expect(displayWrapperValue5.text()).toBe('6')
+
+    await decrementButtonValue5.trigger('click')
+    expect(displayWrapper.text()).toBe('1')
+    expect(displayWrapperValue5.text()).toBe('5')
+  })
+})
---
+++ src/components/__tests__/CounterDisplay.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import CounterDisplay from '../CounterDisplay.vue'
+
+describe('CounterDisplay', () => {
+  it('renders properly without props', () => {
+    const wrapper = mount(CounterDisplay)
+    expect(wrapper.text()).toContain('0')
+    expect(wrapper.find('svg').exists()).toBe(false)
+  })
+  it('renders properly with value', () => {
+    const wrapper = mount(CounterDisplay, { props: { value: '5' } })
+    expect(wrapper.text()).toContain('5')
+    expect(wrapper.find('svg').exists()).toBe(false)
+  })
+  it('renders properly with range set', () => {
+    const wrapper = mount(CounterDisplay, { props: { min: '0', max: '10' } })
+    expect(wrapper.find('svg').exists()).toBe(true)
+  })
+  it('renders properly if duplicated in the page', () => {
+    const wrapper1 = mount(CounterDisplay, { props: { min: '0', max: '10' } })
+    const wrapper2 = mount(CounterDisplay, { props: { counterId: '1' } })
+    expect(wrapper1.find('svg').exists()).toBe(true)
+    expect(wrapper2.find('svg').exists()).toBe(false)
+  })
+})
