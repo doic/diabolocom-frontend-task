8e57afc96bd6aeb756808298ac2e8cf49630df78
Doic <dauhik@gmail.com>
2024-02-23 18:16:51 +0100

    build fixed

--- vite.wc.config.ts
+++ vite.wc.config.ts
@@ -1,24 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ customElement: true })],
+  plugins: [
+    vue({
+      template: { compilerOptions: { isCustomElement: (tag) => tag.includes('-') } }
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
-        index: './index.wc.html'
-      }
-      // output: {
-      //   entryFileNames: '[name].html'
-      // }
+        index: './index.wc.html',
+        main: './src/main.wc.ts'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      },
+      plugins: [
+        copy({
+          targets: [{ src: './dist-wc/index.wc.html', dest: 'dist-wc', rename: 'index.html' }],
+          hook: 'writeBundle'
+        })
+      ]
     },
+
     outDir: 'dist-wc',
     lib: {
       entry: './src/main.wc.ts',
       name: 'web-components',
-      formats: ['es', 'cjs'],
-      fileName: 'lib-wc' //(format) => (format === 'es' ? 'index.js' : 'index.cjs')
+      formats: ['es'],
+      fileName: 'main'
     }
     // sourcemap: true,
     // target: 'esnext',
