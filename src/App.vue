aa2eb631c8f5584740ab57e5c4ef4162e51223e0
Doic <dauhik@gmail.com>
2024-02-25 18:14:34 +0100

    tailwind / css fixes

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,22 @@ import CounterComponent from './components/CounterComponent.ce.vue'
          src="./assets/vue_icon.png" />VueJs Components Version
   </h2>
   <div class="grid md:grid-cols-2 h-full gap-4 place-items-center">
-    <div class="relative flex flex-col min-w-[20rem] w-full max-w-[30rem] bg-primary-200 p-8 gap-4 rounded-md">
-      <CounterDisplay />
-      <CounterButtons />
-      <CounterReset />
-      <LocaleSwitcher />
+    <div>
+      <div class="relative flex flex-col min-w-[20rem] w-full max-w-[30rem] bg-primary-200 p-8 gap-4 rounded-md">
+        <CounterDisplay />
+        <CounterButtons />
+        <CounterReset />
+        <LocaleSwitcher />
+      </div>
     </div>
+    <div>
+      <CounterComponent counterid="1"
+                        min="-100"
+                        max="100"
+                        value="30"
+                        lg="fr"
+                        color="secondary" />
 
-    <CounterComponent counterid="1"
-                      min="-100"
-                      max="100"
-                      value="30"
-                      lg="fr"
-                      color="secondary" />
-
+    </div>
   </div>
 </template>
