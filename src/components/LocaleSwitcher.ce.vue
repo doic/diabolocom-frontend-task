eeca1c6d7b138d2c3336979946997cda56090947
Doic <dauhik@gmail.com>
2024-02-26 09:07:12 +0100

    accessibility

--- src/components/LocaleSwitcher.ce.vue
+++ src/components/LocaleSwitcher.ce.vue
@@ -25,7 +25,7 @@ onMounted(() => {
 			   type="checkbox"
 			   :checked="lg === 'fr'"
 			   class="peer sr-only" />
-		<div
+		<div role="button"
 			 class="relative grid grid-cols-2 transition duration-150 ease-in-out items-center after:absolute  after:h-6 after:w-1/2 after:rounded-md after:bg-white/30 after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-focus:outline-none ">
 			<span class="text-center">English</span>
 			<span class="text-center">Français</span>
